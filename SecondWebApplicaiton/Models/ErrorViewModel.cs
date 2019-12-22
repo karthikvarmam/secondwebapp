a93987a0f00d8d16572a794c56d8d886e2132aea
Karthik Varma Mudunuri <karthik.mudunuri@derivco.co.im>
2019-12-22 02:11:29 +0000

    Add project files.

---
+++ SecondWebApplicaiton/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecondWebApplicaiton.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
